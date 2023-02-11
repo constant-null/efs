db547192e5638499581a05dfd262e90a837adce1
Белоцкий Марк <7520877+constant-null@users.noreply.github.com>
2023-02-11 21:59:01 +0300

    duplicate actor error fixed

--- module/efs-character.js
+++ module/efs-character.js
@@ -1,22 +1,24 @@
 export default class EFSCharacter extends Actor {
     static async create(data, options) {
         const subtype = options?.subtype || "character";
-        data.token = {
-            actorLink: subtype === "character",
-            disposition: subtype === "character" ? 1 : -1,
-            vision: subtype === "character",
-            displayBars: 40,
-            bar1: { attribute: "dp" },
+        if (data.token) {
+            data.token = {
+                actorLink: subtype === "character",
+                disposition: subtype === "character" ? 1 : -1,
+                vision: subtype === "character",
+                displayBars: 40,
+                bar1: {attribute: "dp"},
+            }
         }
 
         if (subtype === "npc") {
-            data.flags = { "core": { "sheetClass": "efs.EFSNPCSheet" } };
+            data.flags = {"core": {"sheetClass": "efs.EFSNPCSheet"}};
         }
 
         await super.create(data, options)
     }
 
-    static async createDialog(data = {}, { parent = null, pack = null, ...options } = {}) {
+    static async createDialog(data = {}, {parent = null, pack = null, ...options} = {}) {
         // Collect data
         const documentName = this.metadata.name;
         const types = ["character", "npc"];
@@ -26,11 +28,11 @@ export default class EFSCharacter extends Actor {
         }
         const folders = parent ? [] : game.folders.filter(f => (f.data.type === documentName) && f.displayed);
         const label = game.i18n.localize(this.metadata.label);
-        const title = game.i18n.format("DOCUMENT.Create", { type: label });
+        const title = game.i18n.format("DOCUMENT.Create", {type: label});
 
         // Render the document creation form
         const html = await renderTemplate(`templates/sidebar/document-create.html`, {
-            name: data.name || game.i18n.format("DOCUMENT.New", { type: label }),
+            name: data.name || game.i18n.format("DOCUMENT.New", {type: label}),
             folder: data.folder,
             folders: folders,
             hasFolders: folders.length >= 1,
@@ -50,11 +52,11 @@ export default class EFSCharacter extends Actor {
             callback: html => {
                 const form = html[0].querySelector("form");
                 const fd = new FormDataExtended(form);
-                foundry.utils.mergeObject(data, fd.toObject(), { inplace: true });
+                foundry.utils.mergeObject(data, fd.toObject(), {inplace: true});
                 if (!data.folder) delete data["folder"];
                 const subtype = data.type;
                 data.type = 'character';
-                return this.create(data, { parent, pack, renderSheet: true, subtype });
+                return this.create(data, {parent, pack, renderSheet: true, subtype});
             },
             rejectClose: false,
             options: options
